0b2706c6b91f87da964582133690b8140fd18761
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-04 14:11:22 +0200

    Added the initial dependecies

---
+++ src/main/java/com/prep/TaskPulse/TaskPulseApplication.java
@@ -0,0 +1,13 @@
+package com.prep.TaskPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prep/TaskPulse/TaskPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep.TaskPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
