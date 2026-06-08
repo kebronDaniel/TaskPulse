5e843fbadaee4d81cb29399b4c143a0a1499dfdf
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-08 13:43:59 +0200

    - modified the port mapping in the docker-compose.yml not to create conflict with the local Db. - also added a dev env't that uses docker container db.

--- src/main/java/com/prep/TaskPulse/TaskPulseApplication.java
+++ src/main/java/com/prep/TaskPulse/TaskPulseApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskPulseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskPulseApplication.class, args);
 	}
-
 }
