56a2621e3a305c250755c80435a7fa29b093a6f7
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-09 15:24:17 +0200

    created a config that would allow jpa auditing across the app.

---
+++ src/main/java/com/prep/taskpulse/config/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.prep.taskpulse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
