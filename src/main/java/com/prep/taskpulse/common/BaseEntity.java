128593dc063b40c99da3f55cc65e792ecd3d6065
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-09 15:23:18 +0200

    created a base entity

---
+++ src/main/java/com/prep/taskpulse/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.prep.taskpulse.common;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    @Version
+    private Long version;
+}
