5bdca32d3001a74074e0a50602662ff69602a514
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-09 15:24:44 +0200

    changed the base package name to taskpulse.

--- src/main/java/com/prep/taskpulse/TaskPulseApplication.java
+++ src/main/java/com/prep/taskpulse/TaskPulseApplication.java
@@ -1,4 +1,4 @@
-package com.prep.TaskPulse;
+package com.prep.taskpulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/prep/taskpulse/TaskPulseApplicationTests.java
+++ src/test/java/com/prep/taskpulse/TaskPulseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.prep.TaskPulse;
+package com.prep.taskpulse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
