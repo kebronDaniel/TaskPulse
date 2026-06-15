8d38265ec8fdbf4e86d1136997cfeb6b379defe0
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-15 15:32:56 +0200

    added specifications for dynamic filters (optional and composable).

---
+++ src/main/java/com/prep/taskpulse/domain/task/repository/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.prep.taskpulse.domain.task.repository;
+
+import com.prep.taskpulse.domain.task.entity.Task;
+import com.prep.taskpulse.domain.task.enums.TaskStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.UUID;
+
+// uses specifications : it allows dynamic filters(optional and composable)
+public interface TaskRepository extends JpaRepository<Task, UUID>, JpaSpecificationExecutor<Task> {
+    Page<Task> findByTaskStatus(TaskStatus taskStatus, Pageable pageable);
+    // relationship traversal
+    List<Task> findByWorkspace_Name(String workspaceName);
+    // combining filters
+    List<Task> findByWorkspace_NameAndTaskStatus(String workspaceName, TaskStatus taskStatus);
+}
