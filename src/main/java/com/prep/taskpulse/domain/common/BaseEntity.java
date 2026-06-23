292d8cfff9829a2cee5f7d49ba60a3c4acf8e638
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-23 12:43:11 +0200

    Added a Notification Entity and also added a few more comments in the base entity.

--- src/main/java/com/prep/taskpulse/domain/common/BaseEntity.java
+++ src/main/java/com/prep/taskpulse/domain/common/BaseEntity.java
@@ -31,10 +31,11 @@ public abstract class BaseEntity {
     private Long version;
 
     @Override
-
     public final boolean equals(Object o) {
         // Task proxy != Task actual class
         if (this == o) return true;
+        // check if two objects are not different(user and Task or ..., they have to be the same type)
+        // This means peeling off of the proxy layer and getting the DB objects.
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         BaseEntity that = (BaseEntity) o;
         return id != null && Objects.equals(id, that.id);
@@ -43,6 +44,7 @@ public abstract class BaseEntity {
     @Override
     public int hashCode() {
         // To generate a constant number for every instance.
+        // this causes hash collision but its handled by the .equals which is clearly implemented.
         return getClass().hashCode();
     }
 }
