9c4597634e8faeaa1283666dc9ee3eb327bec6dd
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-26 16:38:43 +0200

    TaskResponse only responds Id.

--- src/main/java/com/prep/taskpulse/domain/task/dto/TaskResponse.java
+++ src/main/java/com/prep/taskpulse/domain/task/dto/TaskResponse.java
@@ -1,18 +1,5 @@
 package com.prep.taskpulse.domain.task.dto;
 
-import com.prep.taskpulse.domain.task.enums.TaskPriority;
-import com.prep.taskpulse.domain.task.enums.TaskStatus;
-
-import java.time.Instant;
 import java.util.UUID;
 
-public record TaskResponse(
-        UUID id,
-        String title,
-        String description,
-        TaskStatus taskStatus,
-        TaskPriority taskPriority,
-        Instant createdAt,
-        Instant updatedAt
-) {
-}
+public record TaskResponse(UUID id) {}
