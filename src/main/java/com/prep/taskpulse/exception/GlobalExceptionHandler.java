49e4259d5fc5e3cf942142b5a8c8a61bf49f73e0
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-27 15:43:09 +0200

    modified the GlobalExceptionHandler to handle MethodArgumentNotValidException that comes from validation error caused by @valid in the controller.

--- src/main/java/com/prep/taskpulse/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prep/taskpulse/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.prep.taskpulse.exception;
 
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,9 +13,32 @@ import java.time.Instant;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final ServletRequest httpServletRequest;
+
+    public GlobalExceptionHandler(ServletRequest httpServletRequest) {
+        this.httpServletRequest = httpServletRequest;
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleTaskNotFound(ResourceNotFoundException ex){
-        ErrorResponse errorResponse = new ErrorResponse(Instant.now(),HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    public ResponseEntity<ApiErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exception, HttpServletRequest httpServletRequest){
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                exception.getMessage(),
+                httpServletRequest.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest httpServletRequest){
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "request validation error", // you can use map to get the exact field from the exception.
+                httpServletRequest.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
