36a56084cba645521f1377e7b1b087352e153ed6
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-29 15:42:20 +0200

    added a dto to UpdateTaskRequest.

---
+++ src/main/java/com/prep/taskpulse/domain/task/dto/UpdateTaskRequest.java
@@ -0,0 +1,13 @@
+package com.prep.taskpulse.domain.task.dto;
+
+import com.prep.taskpulse.domain.task.enums.TaskPriority;
+
+import java.time.Instant;
+
+public record UpdateTaskRequest(
+        String title,
+        String description,
+        TaskPriority priority,
+        Instant dueDate
+) {
+}
