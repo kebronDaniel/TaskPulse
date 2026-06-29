4dac1cf1590a3047a6aabbc956937e2e0c032a1d
KebronDaniel <kebronjrdaniel@gmail.com>
2026-06-29 15:43:43 +0200

    added a TaskResponse to be a response dto used in the controller.

--- src/main/java/com/prep/taskpulse/domain/task/dto/TaskResponse.java
+++ src/main/java/com/prep/taskpulse/domain/task/dto/TaskResponse.java
@@ -1,5 +1,11 @@
 package com.prep.taskpulse.domain.task.dto;
 
+import com.prep.taskpulse.domain.task.enums.TaskPriority;
+import com.prep.taskpulse.domain.task.enums.TaskStatus;
+
+import java.time.Instant;
 import java.util.UUID;
 
-public record TaskResponse(UUID id) {}
+public record TaskResponse(UUID id, String title,
+                           String description, TaskStatus status,
+                           TaskPriority priority, Instant dueDate) {}
