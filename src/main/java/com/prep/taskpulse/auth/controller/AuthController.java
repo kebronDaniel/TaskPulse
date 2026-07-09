882cf177b8815a4fc6bb0c6b0cb96b8ee2518204
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-09 14:43:42 +0200

    Authcontroller endpoints for login and registering

---
+++ src/main/java/com/prep/taskpulse/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.prep.taskpulse.auth.controller;
+
+import com.prep.taskpulse.auth.dto.AuthResponse;
+import com.prep.taskpulse.auth.dto.LoginRequest;
+import com.prep.taskpulse.auth.dto.RegisterRequest;
+import com.prep.taskpulse.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthResponse register(@Valid @RequestBody RegisterRequest request){
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponse login(@Valid @RequestBody LoginRequest request){
+        return authService.login(request);
+    }
+}
