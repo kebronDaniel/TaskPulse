192cb74e7552a52c6b3ea1c04327cb9a34a37b50
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-09 14:34:46 +0200

    modified repo to include deletedAt column

--- src/main/java/com/prep/taskpulse/domain/user/repository/UserRepository.java
+++ src/main/java/com/prep/taskpulse/domain/user/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.prep.taskpulse.domain.user.repository;
 import com.prep.taskpulse.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
+    boolean existsByEmailAndDeletedAtIsNull(String email);
+    Optional<User> findByIdAndDeletedAtIsNull(UUID id);
 }
