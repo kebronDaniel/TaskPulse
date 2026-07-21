e96336be1581bf674bde40ee411bc370ab1bad21
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-21 16:45:22 +0200

    allow @Configurationproperties for OutboxProperties.

--- src/main/java/com/prep/taskpulse/TaskPulseApplication.java
+++ src/main/java/com/prep/taskpulse/TaskPulseApplication.java
@@ -1,8 +1,13 @@
 package com.prep.taskpulse;
 
+import com.prep.taskpulse.outbox.config.OutboxProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableConfigurationProperties(OutboxProperties.class)
 @SpringBootApplication
 public class TaskPulseApplication {
 	public static void main(String[] args) {
