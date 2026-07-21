6727218df3ee6e1b2fb29b0f5fad35e607470a60
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-21 16:33:21 +0200

    publisher for stale processes.

---
+++ src/main/java/com/prep/taskpulse/outbox/OutboxRecoveryScheduler.java
@@ -0,0 +1,18 @@
+package com.prep.taskpulse.outbox;
+
+import com.prep.taskpulse.outbox.service.OutboxRecoveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxRecoveryScheduler {
+
+    private final OutboxRecoveryService outboxRecoveryService;
+
+    @Scheduled(fixedDelayString = "${taskflow.outbox.recovery-delay-ms:60000}")
+    public void recoverStaleClaim(){
+        outboxRecoveryService.releaseClaims();
+    }
+}
