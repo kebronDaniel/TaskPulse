545f9ba9fa415e7b5e3b28e0168ce4a861f79edd
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-24 15:01:06 +0200

    test the OutboxEventProcessor if it handles success case, open circuit and runtime exception.

--- src/main/java/com/prep/taskpulse/outbox/service/OutboxEventProcessor.java
+++ src/main/java/com/prep/taskpulse/outbox/service/OutboxEventProcessor.java
@@ -1,18 +1,11 @@
 package com.prep.taskpulse.outbox.service;
 
-import com.prep.taskpulse.outbox.OutboxEvent;
 import com.prep.taskpulse.outbox.OutboxMessage;
-import com.prep.taskpulse.outbox.OutboxStatus;
 import com.prep.taskpulse.outbox.publisher.EventPublisher;
-import com.prep.taskpulse.outbox.repository.OutboxRepository;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
-import java.util.concurrent.CompletionException;
 
 @Service
 @RequiredArgsConstructor
@@ -31,7 +24,7 @@ public class OutboxEventProcessor {
         catch (RuntimeException exception){
             Throwable error = rootCause(exception);
             String errorMessage = error.getMessage();
-            if (exception instanceof CallNotPermittedException){
+            if (error instanceof CallNotPermittedException){
                 outboxStatusService.defer(message.eventId(),errorMessage);
             } else {
                 outboxStatusService.recordFailure(message.eventId(),errorMessage);
