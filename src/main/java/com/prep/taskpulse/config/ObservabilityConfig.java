076e5da2b09cb03e8151b8252f7f5dc58c58ff23
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-28 16:39:26 +0200

    added a bean in the ObservabilityConfig to register a metric based on the outbox status

--- src/main/java/com/prep/taskpulse/config/ObservabilityConfig.java
+++ src/main/java/com/prep/taskpulse/config/ObservabilityConfig.java
@@ -1,7 +1,11 @@
 package com.prep.taskpulse.config;
 
 
+import com.prep.taskpulse.outbox.OutboxStatus;
+import com.prep.taskpulse.outbox.repository.OutboxRepository;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +17,22 @@ public class ObservabilityConfig {
     public MeterRegistryCustomizer<MeterRegistry> commonMetricTag(){
         // to add tag to every meter which is used by prometheus.
         return registry -> registry.config().commonTags("service","taskpulse");
+    }
+
+    @Bean
+    public MeterBinder outboxMetrics(OutboxRepository outboxRepository){
+        return  registry -> {
+            registerOutboxGauge(registry,outboxRepository,OutboxStatus.PENDING);
+            registerOutboxGauge(registry,outboxRepository,OutboxStatus.PROCESSING);
+            registerOutboxGauge(registry,outboxRepository,OutboxStatus.FAILED);
+            // unnecessary load to the db if we add also the published.
+        };
+    }
+
+    private void registerOutboxGauge(MeterRegistry registry, OutboxRepository repository, OutboxStatus status){
+        Gauge.builder(
+                "taskflow.outbox.events",repository, repo -> repo.countByStatus(status))
+                .description("Current number of outbox events by status")
+                .tag("status", status.name().toLowerCase()).register(registry);
     }
 }
