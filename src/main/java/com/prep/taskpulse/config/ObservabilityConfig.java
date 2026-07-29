beccc29ae81e1f4e0f12838f8144ed8c3ad6f215
KebronDaniel <kebronjrdaniel@gmail.com>
2026-07-29 15:58:30 +0200

    created a bean to enable kafka mertics observability

--- src/main/java/com/prep/taskpulse/config/ObservabilityConfig.java
+++ src/main/java/com/prep/taskpulse/config/ObservabilityConfig.java
@@ -6,9 +6,13 @@ import com.prep.taskpulse.outbox.repository.OutboxRepository;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.observation.ObservationRegistry;
+import org.hibernate.annotations.Bag;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 public class ObservabilityConfig {
@@ -26,6 +30,20 @@ public class ObservabilityConfig {
             registerOutboxGauge(registry,outboxRepository,OutboxStatus.PROCESSING);
             registerOutboxGauge(registry,outboxRepository,OutboxStatus.FAILED);
             // unnecessary load to the db if we add also the published.
+        };
+    }
+
+    // creates a child span due @observed annotation in the producer
+    // injects the trace and span ID to the child calls.
+    // registers them under one parent.
+    @Bean
+    public InitializingBean enableKafkaTemplateObservation(KafkaTemplate<?,?> kafkaTemplate,
+                                                           ObservationRegistry observationRegistry){
+        return () -> {
+            kafkaTemplate.setObservationRegistry(observationRegistry);
+            // observation-managed timers instead of its older standalone Micrometer timer behavior
+            // this prevents double counting for child spans
+            kafkaTemplate.setObservationEnabled(true);
         };
     }
 
